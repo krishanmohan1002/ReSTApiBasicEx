f3423fcda48cb2ed750a170d5a17033c19487887
krishanmohan1002 <krishanmohan1002@gmail.com>
2023-01-17 15:42:11 +0530

    changes in main class

--- src/main/java/com/example/demo/ReStApiBasicEx12012023Application.java
+++ src/main/java/com/example/demo/ReStApiBasicEx12012023Application.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ReStApiBasicEx12012023Application {
 
+    private static String = "DATA IS NOT SUFFICINET";
+
 	public static void main(String[] args) {
 		SpringApplication.run(ReStApiBasicEx12012023Application.class, args);
 	}
