6c00a21cc489e0ae0d38646a8ef22293162a0c91
krishanmohan1002 <krishanmohan1002@gmail.com>
2023-01-17 16:21:06 +0530

    HIS-321 RESOLVED

--- src/main/java/com/example/demo/ReStApiBasicEx12012023Application.java
+++ src/main/java/com/example/demo/ReStApiBasicEx12012023Application.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ReStApiBasicEx12012023Application {
 
+private static Integer NOOFTRANSACTIONS=100;
+
 	public static void main(String[] args) {
 		SpringApplication.run(ReStApiBasicEx12012023Application.class, args);
 	}
